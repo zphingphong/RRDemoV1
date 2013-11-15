d33f78fe3d4e5727feae2c0c05410a816904bd02
ZoM <pkhantidhara@zinzone.net>
2013-11-15 01:12:12 -0800

    Time and court selection

--- js/page/index.js
+++ js/page/index.js
@@ -6,7 +6,11 @@
  * To change this template use File | Settings | File Templates.
  */
 
-function initialize() {
+$(function() {
+    mapInitialize();
+});
+
+function mapInitialize() {
     window.geocoder = new google.maps.Geocoder();
     var mapOptions = {
         center: new google.maps.LatLng(49.19058, -123.10936),
@@ -16,14 +20,14 @@ function initialize() {
     window.locationSelectionMap = new google.maps.Map(document.getElementById("location-map"), mapOptions);
     getLocationSelectionMarkers();
 }
-google.maps.event.addDomListener(window, 'load', initialize);
 
 function getLocationSelectionMarkers() {
     var addresses = [
         "9151 Van Horne Way, Richmond, BC V6X 1W2",
         "Suite 130-5800 Minoru Blvd, Richmond, BC V6X 2A9",
         "13100 Mitchell Road, Richmond, BC V6V 1M8",
-        "#138-4551 No.3 RD Richmond, B.C, V6X 2C3"
+        "#138-4551 No.3 RD Richmond, B.C, V6X 2C3",
+        "#100-4351 No.3 RD Richmond, B.C, V6X 2C3"
     ];
     $.each(addresses, function(index, address){
         window.geocoder.geocode( { 'address': address}, function(results, status) {
